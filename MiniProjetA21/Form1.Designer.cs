d5118c83e432669fd21f893c40cc8e93e40e2ed6
Kellian'Kizax <57542020+KellianKizax@users.noreply.github.com>
2020-05-12 15:25:05 +0200

    Revert "Merge branch 'master' of https://github.com/Defalt000/MiniProjet_HugoKellian"
    This reverts commit 757f0a35169a68dbf4ac2197e20e9e6a78fa6942, reversing
    changes made to 6e28244f2c2eb3c8bbb28af3498561b807e2ddf3.

--- MiniProjetA21/Form1.Designer.cs
+++ MiniProjetA21/Form1.Designer.cs
@@ -30,11 +30,6 @@
         {
             this.lblTitle = new System.Windows.Forms.Label();
             this.gpbConnec = new System.Windows.Forms.GroupBox();
-            this.lblConnec = new System.Windows.Forms.Label();
-            this.cbUser = new System.Windows.Forms.ComboBox();
-            this.btnNext = new System.Windows.Forms.Button();
-            this.btnExit = new System.Windows.Forms.Button();
-            this.gpbConnec.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -49,60 +44,20 @@
             // 
             // gpbConnec
             // 
-            this.gpbConnec.Controls.Add(this.btnExit);
-            this.gpbConnec.Controls.Add(this.btnNext);
-            this.gpbConnec.Controls.Add(this.cbUser);
-            this.gpbConnec.Controls.Add(this.lblConnec);
             this.gpbConnec.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gpbConnec.Location = new System.Drawing.Point(12, 195);
             this.gpbConnec.Name = "gpbConnec";
-            this.gpbConnec.Size = new System.Drawing.Size(1157, 408);
+            this.gpbConnec.Size = new System.Drawing.Size(1199, 405);
             this.gpbConnec.TabIndex = 1;
             this.gpbConnec.TabStop = false;
             this.gpbConnec.Text = "Bienvenue";
-            // 
-            // lblConnec
-            // 
-            this.lblConnec.AutoSize = true;
-            this.lblConnec.Location = new System.Drawing.Point(111, 75);
-            this.lblConnec.Name = "lblConnec";
-            this.lblConnec.Size = new System.Drawing.Size(161, 23);
-            this.lblConnec.TabIndex = 0;
-            this.lblConnec.Text = "Connectez-vous :";
-            // 
-            // cbUser
-            // 
-            this.cbUser.FormattingEnabled = true;
-            this.cbUser.Location = new System.Drawing.Point(314, 72);
-            this.cbUser.Name = "cbUser";
-            this.cbUser.Size = new System.Drawing.Size(313, 31);
-            this.cbUser.TabIndex = 1;
-            // 
-            // btnNext
-            // 
-            this.btnNext.Location = new System.Drawing.Point(966, 350);
-            this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(185, 52);
-            this.btnNext.TabIndex = 2;
-            this.btnNext.Text = "Exercice Suivant";
-            this.btnNext.UseVisualStyleBackColor = true;
-            // 
-            // btnExit
-            // 
-            this.btnExit.Location = new System.Drawing.Point(734, 350);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(173, 52);
-            this.btnExit.TabIndex = 3;
-            this.btnExit.Text = "Quitter";
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // frmStart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 38F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
-            this.ClientSize = new System.Drawing.Size(1181, 615);
+            this.ClientSize = new System.Drawing.Size(1223, 612);
             this.Controls.Add(this.gpbConnec);
             this.Controls.Add(this.lblTitle);
             this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 19.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -110,8 +65,6 @@
             this.Name = "frmStart";
             this.Text = "LA CASA DE BABBEL";
             this.Load += new System.EventHandler(this.frmStart_Load);
-            this.gpbConnec.ResumeLayout(false);
-            this.gpbConnec.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,10 +74,6 @@
 
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.GroupBox gpbConnec;
-        private System.Windows.Forms.ComboBox cbUser;
-        private System.Windows.Forms.Label lblConnec;
-        private System.Windows.Forms.Button btnNext;
-        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- MiniProjetA21/Form1.cs
+++ MiniProjetA21/Form1.cs
@@ -7,10 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.OleDb;
 
 namespace MiniProjetA21
-{ 
+{
     public partial class frmStart : Form
     {
         public frmStart()
@@ -18,65 +17,9 @@ namespace MiniProjetA21
             InitializeComponent();
         }
 
-        //Definition chaine de connection à la base
-        string chaine = @"Provider=Microsoft.Jet.OLEDB.4.0;
-        Data Source=F:\Documents\Visual Studio 2019\MiniProjet_HugoKellian\MiniProjetA21\baseLangue.mdb";
-
-        //Initialisation de la connection
-        OleDbConnection connec = new OleDbConnection();
-
         private void frmStart_Load(object sender, EventArgs e)
         {
-            try
-            {
 
-                //Connection avec la chaine
-                connec.ConnectionString = chaine;
-                //Ouverture de la connection
-                connec.Open();
-
-                string requete = @"select [pnUtil],[nomUtil] from Utilisateurs";
-
-                //Paramètrage de l'objet commande
-                OleDbCommand cmd = new OleDbCommand();
-                cmd.Connection = connec;
-                cmd.CommandType = CommandType.Text;
-                cmd.CommandText = requete;
-
-                //Execution de la requète
-                OleDbDataReader dr = cmd.ExecuteReader();
-
-                //Tant que le résultat n'est pas vide
-                while (dr.Read())
-                {
-                    //On ajoute le prénom et nom à cbUser
-                    cbUser.Items.Add(dr.GetString(0) + " " + dr.GetString(1));
-                }
-            }
-
-            catch (InvalidOperationException)
-            {
-                //Erreur de connection à la base
-                MessageBox.Show("Erreur de connection à la base de données");
-            }
-
-            catch (OleDbException)
-            {
-                //Erreur requète SQL
-                MessageBox.Show("Erreur dans la requète SQL");
-            }
-
-            catch (Exception erreur)
-            {
-                //Exception inconnue
-                MessageBox.Show("Autre erreur : " + erreur.GetType().ToString());
-            }
-        }
-
-        private void btnExit_Click(object sender, EventArgs e)
-        {
-            //Fermeture du formulaire
-            Application.Exit();
         }
     }
 }
