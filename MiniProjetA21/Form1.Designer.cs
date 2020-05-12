7d3e8f649a3c54233107e18a2e725475f84eb4e5
Hugo Barbi <hugo.barbi.pro@gmail.com>
2020-05-12 14:22:36 +0200

    formBackground

--- MiniProjetA21/Form1.Designer.cs
+++ MiniProjetA21/Form1.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
             this.ClientSize = new System.Drawing.Size(1179, 560);
             this.Name = "frmStart";
             this.Text = "LA CASA DE BABBEL";
