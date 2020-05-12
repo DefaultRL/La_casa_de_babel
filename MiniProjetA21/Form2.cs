6e28244f2c2eb3c8bbb28af3498561b807e2ddf3
Kellian'Kizax <57542020+KellianKizax@users.noreply.github.com>
2020-05-12 15:16:28 +0200

    Constructeur Form2 Phrases a trous

--- MiniProjetA21/Form2.cs
+++ MiniProjetA21/Form2.cs
@@ -4,17 +4,36 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MiniProjetA21
 {
-    public partial class Form2 : Form
+    public partial class Phrases_a_trous : Form
     {
-        public Form2()
+        public DataSet tables;
+        public int numCours;
+        public int numLecon;
+        public int numExo;
+
+        /* Phrases_a_trous  :   constructeur du formulaire
+         *      ds              :   DataSet :   dataset local de l'ensemble des tables
+         *      numeroExercice  :   int     :   indique le numéro de l'exercice courant
+         */
+        public Phrases_a_trous(DataSet ds, int cours, int lecon, int exo)
         {
+            tables = ds;
+            numCours = cours;
+            numLecon = lecon;
+            numExo = exo;
             InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Text = "";
         }
     }
 }
