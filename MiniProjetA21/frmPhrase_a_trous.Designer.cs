864d629155ce58124fa931043c99d2fadf61e36e
Hugo Barbi <hugo.barbi.pro@gmail.com>
2020-05-26 15:48:59 +0200

    debugGit

---
+++ MiniProjetA21/frmPhrase_a_trous.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MiniProjetA21
+{
+    partial class frmPhrases_a_trous
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitlePhrases_Trous = new System.Windows.Forms.Label();
+            this.gpbPhrases_Trous = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnVerif = new System.Windows.Forms.Button();
+            this.txbMot1 = new System.Windows.Forms.TextBox();
+            this.lblEnonce = new System.Windows.Forms.Label();
+            this.lblPhrase = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.gpbPhrases_Trous.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitlePhrases_Trous
+            // 
+            this.lblTitlePhrases_Trous.AutoSize = true;
+            this.lblTitlePhrases_Trous.Location = new System.Drawing.Point(420, 35);
+            this.lblTitlePhrases_Trous.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblTitlePhrases_Trous.Name = "lblTitlePhrases_Trous";
+            this.lblTitlePhrases_Trous.Size = new System.Drawing.Size(295, 32);
+            this.lblTitlePhrases_Trous.TabIndex = 1;
+            this.lblTitlePhrases_Trous.Text = "LA CASA DE BABBEL";
+            // 
+            // gpbPhrases_Trous
+            // 
+            this.gpbPhrases_Trous.Controls.Add(this.button2);
+            this.gpbPhrases_Trous.Controls.Add(this.btnVerif);
+            this.gpbPhrases_Trous.Controls.Add(this.txbMot1);
+            this.gpbPhrases_Trous.Controls.Add(this.lblEnonce);
+            this.gpbPhrases_Trous.Controls.Add(this.lblPhrase);
+            this.gpbPhrases_Trous.Controls.Add(this.btnExit);
+            this.gpbPhrases_Trous.Controls.Add(this.btnNext);
+            this.gpbPhrases_Trous.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbPhrases_Trous.Location = new System.Drawing.Point(12, 100);
+            this.gpbPhrases_Trous.Name = "gpbPhrases_Trous";
+            this.gpbPhrases_Trous.Size = new System.Drawing.Size(1157, 494);
+            this.gpbPhrases_Trous.TabIndex = 2;
+            this.gpbPhrases_Trous.TabStop = false;
+            this.gpbPhrases_Trous.Text = "Phrase à trous";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(978, 334);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 34);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Afficher Solution";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnVerif
+            // 
+            this.btnVerif.Location = new System.Drawing.Point(978, 374);
+            this.btnVerif.Name = "btnVerif";
+            this.btnVerif.Size = new System.Drawing.Size(173, 34);
+            this.btnVerif.TabIndex = 7;
+            this.btnVerif.Text = "Vérifier";
+            this.btnVerif.UseVisualStyleBackColor = true;
+            this.btnVerif.Click += new System.EventHandler(this.btnVerif_Click);
+            // 
+            // txbMot1
+            // 
+            this.txbMot1.BackColor = System.Drawing.Color.White;
+            this.txbMot1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbMot1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txbMot1.Location = new System.Drawing.Point(194, 162);
+            this.txbMot1.Name = "txbMot1";
+            this.txbMot1.Size = new System.Drawing.Size(100, 23);
+            this.txbMot1.TabIndex = 6;
+            // 
+            // lblEnonce
+            // 
+            this.lblEnonce.AutoSize = true;
+            this.lblEnonce.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnonce.Location = new System.Drawing.Point(52, 70);
+            this.lblEnonce.Name = "lblEnonce";
+            this.lblEnonce.Size = new System.Drawing.Size(393, 31);
+            this.lblEnonce.TabIndex = 5;
+            this.lblEnonce.Text = "Complétez les mots manquants :";
+            // 
+            // lblPhrase
+            // 
+            this.lblPhrase.AutoSize = true;
+            this.lblPhrase.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhrase.Location = new System.Drawing.Point(110, 165);
+            this.lblPhrase.Name = "lblPhrase";
+            this.lblPhrase.Size = new System.Drawing.Size(78, 16);
+            this.lblPhrase.TabIndex = 4;
+            this.lblPhrase.Text = "phrases";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(978, 454);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(173, 34);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Quitter";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(978, 414);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(173, 34);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Exercice Suivant";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // frmPhrases_a_trous
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(1182, 606);
+            this.Controls.Add(this.gpbPhrases_Trous);
+            this.Controls.Add(this.lblTitlePhrases_Trous);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 19.8F, System.Drawing.FontStyle.Italic);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "frmPhrases_a_trous";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.gpbPhrases_Trous.ResumeLayout(false);
+            this.gpbPhrases_Trous.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitlePhrases_Trous;
+        private System.Windows.Forms.GroupBox gpbPhrases_Trous;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lblEnonce;
+        private System.Windows.Forms.Label lblPhrase;
+        private System.Windows.Forms.TextBox txbMot1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnVerif;
+    }
+}
---
+++ MiniProjetA21/frmPhrase_a_trous.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniProjetA21
+{
+    public partial class frmPhrases_a_trous : Form
+    {
+        public DataSet tables;
+        public string numCours;
+        public int numLecon;
+        public int numExo;
+        List<string> liste_motsManquants = new List<string>();
+
+        /* Phrases_a_trous  :   constructeur du formulaire
+         *      ds              :   DataSet :   dataset local de l'ensemble des tables
+         *      numeroExercice  :   int     :   indique le numero de l'exercice courant
+         */
+        public frmPhrases_a_trous(DataSet ds, string cours, int lecon, int exo)
+        {
+            tables = ds;
+            numCours = cours;
+            numLecon = lecon;
+            numExo = exo;
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            int codePhrase = -1;
+            string numMots = string.Empty;
+            string textePhrase = string.Empty;
+            string traducPhrase = string.Empty;
+            List<int> liste_numMots = new List<int>();
+            
+
+
+
+            // parcours de la table <Cours> afin de trouver son titre et de l'afficher en en-tete de fenetre
+            foreach (DataRow dr in tables.Tables["Cours"].Rows)
+            {
+                if (dr[0].ToString() == numCours)
+                    this.Text = dr[1].ToString();
+            }
+
+            // parcours de la table <Exercices> afin de trouver les informations necessaires
+            foreach(DataRow dr in tables.Tables["Exercices"].Rows)
+            {
+                bool a = int.Parse(dr["numExo"].ToString()) == numExo;
+                bool b = dr["numCours"].ToString() == numCours;
+                bool c = int.Parse(dr["numLecon"].ToString()) == numLecon;
+
+                if (a && b && c)
+                {
+                    // on recupere ici l'enonce, le code de la phrase et les mots a completer
+                    lblEnonce.Text = dr["enonceExo"].ToString();
+                    codePhrase = int.Parse(dr["codePhrase"].ToString());
+                    numMots = dr["listeMots"].ToString();
+                }
+            } // fin foreach <Exercices>
+
+            // parcours de la table <Phrases> afin de trouver les informations necessaires
+            foreach(DataRow dr in tables.Tables["Phrases"].Rows)
+            {
+                if (int.Parse(dr[0].ToString()) == codePhrase)
+                {
+                    textePhrase = dr["textePhrase"].ToString();
+                    traducPhrase = dr["traducPhrase"].ToString();
+                }
+            } // fin foreach <Phrases>
+
+
+
+            // on recupere la liste de mots a completer
+            liste_numMots = numMots.Split('/').Select(int.Parse).ToList();
+            
+            foreach(int i in liste_numMots)
+            {
+                liste_motsManquants.Add( textePhrase.Split(' ')[i] );
+            }
+
+
+
+            // generation de la phrase a afficher dans le label, sans les mots a completer
+            // generation dynamique des textBox
+            // 110 165 location label
+            // dimensions label : 10 pxl par charactere + 4 pxl
+            string temp = string.Empty;
+            int location_txb = 114;
+            foreach(string str in textePhrase.Split(' '))
+            {
+                if ( liste_motsManquants.Contains(str))
+                {
+                    temp += ' ';
+                    location_txb += 10;
+
+                    TextBox txbMot = new TextBox();
+
+                    txbMot.BackColor = System.Drawing.Color.White;
+                    txbMot.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    txbMot.Location = new System.Drawing.Point(location_txb, 162);
+                    txbMot.Name = "txbMot_" + str;
+                    txbMot.AutoSize = true;
+                    txbMot.TabIndex = 6;
+
+                    gpbPhrases_Trous.Controls.Add(txbMot);
+
+                    // on remplace les mots manquants par des espaces
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        temp += ' ';
+                        location_txb += 10;
+                    }
+                    
+                    temp += ' ';
+                    location_txb += 10;
+                }
+                else
+                {
+                    temp += str;
+                    // 1 charactere = 10 pxl
+                    location_txb += str.Length*10;
+                }
+                temp += ' ';
+            } // fin foreach textePhrase
+            lblPhrase.Text = temp;
+        }
+
+        private void btnVerif_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach(Control ctrl in gpbPhrases_Trous.Controls)
+            {
+                if(ctrl is TextBox)
+                {
+                    if(ctrl.Text == liste_motsManquants[i])
+                    {
+                        ctrl.BackColor = System.Drawing.Color.Green;
+                    }
+                    else
+                    {
+                        ctrl.BackColor = System.Drawing.Color.Red;
+                    }
+                    i++;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach (Control ctrl in gpbPhrases_Trous.Controls)
+            {
+                if (ctrl is TextBox)
+                {
+                    ctrl.Text = liste_motsManquants[i];
+                    i++;
+                }
+            }
+        }
+    }
+}
