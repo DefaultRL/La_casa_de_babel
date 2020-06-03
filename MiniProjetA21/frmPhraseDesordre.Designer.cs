d150b5244ee80c27631709550eba1435ca0fdb31
Hugo Barbi <hugo.barbi.pro@gmail.com>
2020-06-03 12:10:29 +0200

    addPhraseDesordre

--- MiniProjetA21/frmPhraseDesordre.Designer.cs
+++ MiniProjetA21/frmPhraseDesordre.Designer.cs
@@ -30,10 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.gbDesordre = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnSoluc = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
-            this.lblTradcPhrase = new System.Windows.Forms.Label();
+            this.lblTraducPhrase = new System.Windows.Forms.Label();
             this.btnQuitter = new System.Windows.Forms.Button();
             this.gbDesordre.SuspendLayout();
             this.SuspendLayout();
@@ -51,10 +50,9 @@
             // gbDesordre
             // 
             this.gbDesordre.Controls.Add(this.btnQuitter);
-            this.gbDesordre.Controls.Add(this.textBox1);
             this.gbDesordre.Controls.Add(this.btnSoluc);
             this.gbDesordre.Controls.Add(this.btnReset);
-            this.gbDesordre.Controls.Add(this.lblTradcPhrase);
+            this.gbDesordre.Controls.Add(this.lblTraducPhrase);
             this.gbDesordre.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbDesordre.Location = new System.Drawing.Point(12, 220);
             this.gbDesordre.Name = "gbDesordre";
@@ -62,13 +60,6 @@
             this.gbDesordre.TabIndex = 1;
             this.gbDesordre.TabStop = false;
             this.gbDesordre.Text = "Remettez les éléments dans l\'ordre";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(27, 138);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 30);
-            this.textBox1.TabIndex = 3;
             // 
             // btnSoluc
             // 
@@ -88,14 +79,14 @@
             this.btnReset.Text = "Rétablir";
             this.btnReset.UseVisualStyleBackColor = true;
             // 
-            // lblTradcPhrase
+            // lblTraducPhrase
             // 
-            this.lblTradcPhrase.AutoSize = true;
-            this.lblTradcPhrase.Location = new System.Drawing.Point(74, 69);
-            this.lblTradcPhrase.Name = "lblTradcPhrase";
-            this.lblTradcPhrase.Size = new System.Drawing.Size(42, 23);
-            this.lblTradcPhrase.TabIndex = 0;
-            this.lblTradcPhrase.Text = "N/A";
+            this.lblTraducPhrase.AutoSize = true;
+            this.lblTraducPhrase.Location = new System.Drawing.Point(74, 69);
+            this.lblTraducPhrase.Name = "lblTraducPhrase";
+            this.lblTraducPhrase.Size = new System.Drawing.Size(42, 23);
+            this.lblTraducPhrase.TabIndex = 0;
+            this.lblTraducPhrase.Text = "N/A";
             // 
             // btnQuitter
             // 
@@ -130,11 +121,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblTradcPhrase;
+        private System.Windows.Forms.Label lblTraducPhrase;
         private System.Windows.Forms.GroupBox gbDesordre;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Button btnSoluc;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnQuitter;
     }
 }
--- MiniProjetA21/frmPhraseDesordre.cs
+++ MiniProjetA21/frmPhraseDesordre.cs
@@ -25,21 +25,50 @@ namespace MiniProjetA21
             numCours = cours;
             numLecon = lecon;
             numExo = exo;
-            MessageBox.Show("User : " + numCours + " " + numLecon + " " + numExo);
         }
 
         private void frmPhraseDesordre_Load(object sender, EventArgs e)
         {
             List<string> mots = new List<string>();
 
+            //Récupération du codePhrase dans la table Exercices
             string filtreExo = @"[numCours] = '" + numCours + "' and [numLecon] = " + numLecon + 
                              " and [numExo] = " + numExo;
             DataRow[] tabRes = ds.Tables["Exercices"].Select(filtreExo);
 
+            //Récupération de traducPhrase dans la table Phrase
             string filtrePhrase = @"[codePhrase] = " + tabRes[0][0];
             DataRow[] tabTraduc = ds.Tables["Phrases"].Select(filtrePhrase);
 
-            MessageBox.Show(tabTraduc[0][2].ToString());
+            lblTraducPhrase.Text = tabTraduc[0][2].ToString();
+
+            string textPhrase = tabTraduc[0][1].ToString();
+
+            char separator = ' ';
+            string[] tabMots = textPhrase.Split(separator);
+
+            Random rd = new Random();
+            int gauche = 30;
+            int top = 140;
+
+            for (int i = 0; i < tabMots.Length; i++)
+            {
+                TextBox tb = new TextBox();
+                tb.Tag = i;
+                tb.ReadOnly = true;
+                tb.Left = gauche;
+                tb.Top = top;
+                gauche += 120;
+
+                if( i > 8)
+                {
+                    top += 50;
+                }
+                
+                tb.Text = tabMots[rd.Next(tabMots.Length - 1)];
+
+                gbDesordre.Controls.Add(tb);
+            }
         }
 
         private void btnQuitter_Click(object sender, EventArgs e)
